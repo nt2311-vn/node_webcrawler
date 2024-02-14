97a9f87b15f83b03536a6deae07540bea032e621
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-14 23:45:47 +0700

    Implement main file

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const { normalizeURL } = require("./crawl.js");
+function main() {
+	if (process.argv.length < 3) {
+		console.log("No website provided");
+		process.exit(1);
+	}
 
-console.log(normalizeURL("https://google.com.vn"));
+	console.log("Start crawling web");
+}
+
+main();
