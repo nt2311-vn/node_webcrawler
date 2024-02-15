f4e9530e2189e7813338bb9a55c0be9e9d90e92f
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-15 08:52:57 +0700

    Finish the crawling project

--- report.js
+++ report.js
@@ -1,32 +1,32 @@
 /**
+ * Sort the pages objects
+ * @param {object} pages - The page objects contain url we crawled
+ * @returns {Array} Returns the array of url sorted
+ */
+const sortPages = (pages) => {
+	const result = Object.entries(pages).sort((a, b) => b[1] - a[1]);
+
+	return result;
+};
+
+/**
  * Print and format the reprot of our crawling
  * @param {Array} pages - The sorted pages as array
  * @returns {void} Return nothing and this is mainly formatted utility
  */
 
 const printReport = (pages) => {
-	for (const page of pages) {
+	const sortedPages = sortPages(pages);
+	console.log("==================");
+	console.log("------REPORT------");
+	console.log("==================");
+	for (const page of sortedPages) {
 		const [pageURL, pageAppear] = page;
-		console.log("==================");
-		console.log("REPORT");
-		console.log("==================");
 		console.log(`Found ${pageURL} ${pageAppear} times`);
-		console.log("==================");
-		console.log("END REPORT");
-		console.log("==================");
 	}
-};
-
-/**
- * Sort the pages objects
- * @param {object} pages - The page objects contain url we crawled
- * @returns {Array} Returns the array of url sorted
- */
-
-const sortPages = (pages) => {
-	const result = Object.entries(pages).sort((a, b) => b[1] - a[1]);
-
-	return result;
+	console.log("==================");
+	console.log("----END REPORT----");
+	console.log("==================");
 };
 
 module.exports = {
