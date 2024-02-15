c29fd342b5001fb543be3526542205782690220a
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-15 08:06:57 +0700

    Execute main entry finished and expected after mark main as async

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require("./crawl.js");
 
-function main() {
+async function main() {
 	if (process.argv.length < 3) {
 		console.log("No website provided");
 		process.exit(1);
@@ -14,7 +14,7 @@ function main() {
 	const baseURL = process.argv[2];
 	console.log(`Start crawling website:${baseURL}`);
 
-	const pages = crawlPage(baseURL, baseURL, {});
+	const pages = await crawlPage(baseURL, baseURL, {});
 
 	for (const page of Object.entries(pages)) {
 		console.log(page);
