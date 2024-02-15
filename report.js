07d9b2b92e50c7cea95c7acfac7916bd8e8eff60
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-15 08:11:31 +0700

    Implement report and test report module

---
+++ report.js
@@ -0,0 +1,13 @@
+/**
+ * Sort the pages objects
+ * @param {object} pages - The pages object contain url we crawled
+ * @returns {Array} Returns the array of url sorted
+ */
+
+const sortPages = (pages) => {
+	return [];
+};
+
+module.exports = {
+	sortPages,
+};
